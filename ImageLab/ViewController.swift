311aa7f03f9df78fad6ca23689c0f55e08ee6d26
Eric Larson <eclarson@erics-macbook-pro.local>
2016-09-09 16:30:06 -0500

    initial commit

--- ImageLab/ViewController.swift
+++ ImageLab/ViewController.swift
@@ -8,30 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+class ViewController: UIViewController   {
 
-    @IBOutlet weak var imageView: UIImageView!
+    
     var filters : [CIFilter]! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let urlPath = NSBundle.mainBundle().pathForResource("smu-campus", ofType: "jpg")
-        let fileURL = NSURL.fileURLWithPath(urlPath!)
-        
-        let beginImage = CIImage(contentsOfURL: fileURL)
-        
         self.setupFilters()
         
-        let newImage = UIImage(CIImage: applyFilters(beginImage!))
-        self.imageView.image = newImage
     
     }
     
     func setupFilters(){
         filters = []
         let filterBloom = CIFilter(name: "CIBloom")!
-        //filterBloom.setValue(beginImage, forKey: kCIInputImageKey)
         filterBloom.setValue(0.5, forKey: kCIInputIntensityKey)
         filterBloom.setValue(20, forKey: "inputRadius")
         filters.append(filterBloom)
@@ -50,29 +41,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         return retImage
     }
     
-    
-    @IBAction func loadImage(sender: UIButton) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.sourceType = .Camera
-        self.presentViewController(picker, animated: true, completion: nil)
-        
-    }
-    
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
-        let image = info["UIImagePickerControllerOriginalImage"] as! UIImage
-        let beginImage = CIImage(image: image)
-        let newImage = UIImage(CIImage: applyFilters(beginImage!), scale: CGFloat(1.0), orientation: image.imageOrientation)
-        self.imageView.image = newImage
-        
-    }
 
 }
 
