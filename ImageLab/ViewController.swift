a49d14918cd81320cfd7ff8cf3b37598201b18aa
Eric Larson <eclarson@erics-macbook-pro.local>
2016-09-09 16:04:51 -0500

    taking images via camera

--- ImageLab/ViewController.swift
+++ ImageLab/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     @IBOutlet weak var imageView: UIImageView!
+    var filter : CIFilter! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,7 +21,7 @@ class ViewController: UIViewController {
         
         let beginImage = CIImage(contentsOfURL: fileURL)
         
-        let filter = CIFilter(name: "CIBloom")!
+        filter = CIFilter(name: "CIBloom")!
         filter.setValue(beginImage, forKey: kCIInputImageKey)
         filter.setValue(0.5, forKey: kCIInputIntensityKey)
         filter.setValue(20, forKey: "inputRadius")
@@ -28,7 +30,31 @@ class ViewController: UIViewController {
         self.imageView.image = newImage
     
     }
+    
+    
+    @IBAction func loadImage(sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .Camera
+        self.presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        let image = info["UIImagePickerControllerOriginalImage"] as! UIImage
+        let beginImage = CIImage(image: image)
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        let newImage = UIImage(CIImage: filter.outputImage!, scale: CGFloat(1.0), orientation: image.imageOrientation)
+        self.imageView.image = newImage
+        
+    }
 
 }
 
